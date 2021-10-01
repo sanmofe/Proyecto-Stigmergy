34be3dbdcf920ad88fc1b17ea8bad11156e98b48
Brenda-cbp <bc.barahona@uniandes.edu.co>
2021-10-01 01:42:28 -0500

    Modelos finalizados

--- comprador/admin.py
+++ comprador/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from .models import Comprador
 # Register your models here.
+admin.site.register(Comprador)
--- restaurante/admin.py
+++ restaurante/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Restaurante
 # Register your models here.
+
+admin.site.register(Restaurante)

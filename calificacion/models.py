fa2a30c0c885b61b422e7fd6e2fc4d4bc3ee79fa
Brenda-cbp <bc.barahona@uniandes.edu.co>
2021-10-01 01:10:15 -0500

    changes :)

--- calificacion/models.py
+++ calificacion/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Calificacion(models.Model):
+	"""docstring for Calificacion
+"""
+"""
+	def __init__(self, arg):
+		super(Calificacion
+		, self).__init__()
+		self.arg = arg
+"""	
+	comentario =models.CharField(max_length=150)
+	estrellas=models.FloatField(null= True, blank=True, default=None)
+	sabor =models.CharField(max_length=150)
+	
+	def __str__(self):
+		return '%s %s' % (self.comentario, self.estrellas)
+        return '{}'.format(self.sabor)
--- producto/models.py
+++ producto/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Producto(models.Model):
+	"""docstring for Producto"""
+	"""
+	def __init__(self, arg):
+		super(Producto, self).__init__()
+		self.arg = arg
+	"""
+	nombre =models.CharField(max_length=50)
+	precio =models.FloatField(null= True, blank=True, default=None)
+	informacion=models.CharField(max_length=150)
+
+	def __str__(self):
+        return '%s %s' % (self.nombre, self.precio)
+        return '{}'.format(self.informacion)

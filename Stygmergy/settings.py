a587efeca474d3e9e6a0963abf1dce86911d1762
Miguel Angel Acosta Walteros <ma.acostaw@uniandes.edu.co>
2021-11-18 23:58:09 +0000

    Updated auth.py

--- Stygmergy/settings.py
+++ Stygmergy/settings.py
@@ -147,6 +147,6 @@ SOCIAL_AUTH_AUTH0_SCOPE = [
  'role',
 ]
 AUTHENTICATION_BACKENDS = {
- 'monitoring.auth0backend.Auth0',
+ 'stygmergy.auth0backend.Auth0',
  'django.contrib.auth.backends.ModelBackend',
 }

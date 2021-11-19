b904b741e8cb37ec179c96d1375d352e0e1b5926
sanmofe <santiagomorafelix@gmail.com>
2021-11-18 21:24:28 -0500

    Path y view de crear restaurantes

--- restaurante/urls.py
+++ restaurante/urls.py
@@ -5,4 +5,5 @@ urlpatterns=[
     path('restaurante/list/',views.get_resturantes, name= 'restauranteList'),
     path('restaurante/get/<int:id>/', views.get_resturante, name= 'restaurante'),
     path('restaurante/delete/<int:id>/', views.delete_resturante, name= 'deleteRestaurante'), 
+    path('create/', views.create_restaurante, name='createRestaurante')
 ]
--- restaurante/views.py
+++ restaurante/views.py
@@ -2,19 +2,36 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.core import serializers
 from .logic import logic_restaurante
+from django.contrib import messages
+from .forms import RestauranteForm
 
 # Create your views here.
 def get_resturantes(request):
-    resturantes = logic_resturante.get_all_resturantes()
+    resturantes = logic_restaurante.get_all_resturantes()
     resturantes_list = serializers.serialize('json', resturantes)
     return HttpResponse(resturantes_list, content_type='application/json')
 
 def get_resturante(request, id):
-    resturante = logic_resturante.get_resturante(id)
+    resturante = logic_restaurante.get_resturante(id)
     resturante_rta= serializers.serialize('json', resturante)
     return HttpResponse(resturante_rta, content_type='application/json')
 
 def delete_resturante(request, id):
-    resturante = logic_resturante.delete_resturante(id)
+    resturante = logic_restaurante.delete_resturante(id)
     resturante_rta= serializers.serialize('json', resturante)
     return HttpResponse(resturante_rta, content_type='application/json')
+
+
+def create_restaurante(request, id):
+    if request.method == 'POST':
+        form = RestauranteForm(request.POST)
+        if form.is_valid():
+            logic_restaurante.create_restaurante(form)
+            messages.add_message(request, messages.SUCCESS, 'Restaurante creado')
+        else:
+            print(form.errors)
+    else:
+        form = RestauranteForm()
+    return render(request, 'create_pedido.html', {'form':form})
+        
+        

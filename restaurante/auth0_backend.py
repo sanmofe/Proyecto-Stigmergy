6c49dc4cd0f1a09062f52dddcec0bb66542ae078
maacostaw <ma.acostaw@uniandes.edu.co>
2021-11-18 19:03:02 -0500

    ads

--- restaurante/auth0_backend.py
+++ restaurante/auth0_backend.py
@@ -29,10 +29,10 @@ class Auth0(BaseOAuth2):
         resp = requests.get(url, headers=headers)
         userinfo = resp.json()
 
-    return {'username': userinfo['nickname'],
-        'first_name': userinfo['name'],
-        'picture': userinfo['picture'],
-        'user_id': userinfo['sub']}
+        return {'username': userinfo['nickname'],
+            'first_name': userinfo['name'],
+            'picture': userinfo['picture'],
+            'user_id': userinfo['sub']}
 
     def getRole(request):
         user = request.user

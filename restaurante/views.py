b0f01d0695bedada2018c62a60a21c6ad5043bd6
sanmofe <santiagomorafelix@gmail.com>
2021-11-19 00:19:22 -0500

    skiojdhuiasgfduiy8sdgfruyiahgsy

--- restaurante/views.py
+++ restaurante/views.py
@@ -57,6 +57,6 @@ def restaurante_create(request):
         context = {
             'form' : form,
         }
-        return render(request, 'restaurante/list', context)
+        return render(request, 'templates/restaurante.html', context)
     else:
         return HttpResponse("Unauthorized User")
